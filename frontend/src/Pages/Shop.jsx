5ed1fc6ed86546d7fdd8dc42bde8d0ad830dd1d3
bagyalakshmi-velu <bagyaniranjan1326@gmail.com>
2025-12-27 22:05:52 +0530

    Fix import path for Offers component

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 
 import HeroCarousel from '../Components/Hero/HeroCarosal';
 import Popular from '../Components/Popular/Popular';
-import Offers from '../Components/Offers/offers';
+import Offers from '../Components/Offers/Offers';
 import NewCollections from '../Components/NewCollections/NewCollections';
 import NewsLetter from '../Components/NewsLetter/NewsLetter';
-
 
 const Shop = () => {
   return (
     <div>
-      <HeroCarousel/>
-      <Popular/>
-      <Offers/>
-      <NewCollections/>
-      <NewsLetter/>
-      
+      <HeroCarousel />
+      <Popular />
+      <Offers />
+      <NewCollections />
+      <NewsLetter />
     </div>
   );
-}
+};
 
 export default Shop;
